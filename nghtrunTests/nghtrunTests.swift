2757c2a4299bd0fcb38427f8520c86970180d865
ozgungungor <ozgun.gungor>
2025-11-05 21:05:31 +0300

    Initial Commit

---
+++ nghtrunTests/nghtrunTests.swift
@@ -0,0 +1,17 @@
+//
+//  nghtrunTests.swift
+//  nghtrunTests
+//
+//  Created by özgün güngör on 5.11.2025.
+//
+
+import Testing
+@testable import nghtrun
+
+struct nghtrunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ nghtrunUITests/nghtrunUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  nghtrunUITestsLaunchTests.swift
+//  nghtrunUITests
+//
+//  Created by özgün güngör on 5.11.2025.
+//
+
+import XCTest
+
+final class nghtrunUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
